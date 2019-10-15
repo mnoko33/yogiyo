72aa06b5a38cba1a0b062e64e526dce710a37a98
sangtaek lee <srs99125@naver.com>
2019-10-15 12:14:35 +0900

    apply jwt

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,14 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const authMiddleware = require('./middlewares/auth');
+const jwt = require('jsonwebtoken');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
+const infoRouter = require('./routes/info');
 
 const app = express();
 
@@ -32,6 +35,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/*', authMiddleware);
+app.use('/api/info', infoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
