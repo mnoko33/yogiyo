fb2d1bd15d191473b62d1be5e6f9259029c88e1b
sangtaek lee <srs99125@naver.com>
2019-10-15 14:08:22 +0900

    edit creating jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,20 @@ const axios = require('axios');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
+// token 발행 함수
+async function createJWT(user) {
+    const secret_key = "ssaudy";
+    const payload = {
+        "sub": "ssaudy",
+        "id": user.id,
+        "username": user.username,
+        "address": user.address,
+        "location": user.location,
+        "phone_number": user.phone_num,
+    };
+    return jwt.sign(payload, secret_key);
+}
+
 // 회원가입
 router.post('/signup', async function(req, res, next) {
     const username = req.body.username;
@@ -16,11 +30,23 @@ router.post('/signup', async function(req, res, next) {
         username: username,
         password: password,
         phone_num: phone_num
-    }).then(() => {
-        res.json({
-            "status": true,
-            "message": "회원가입이 완료됐습니다."
-        })
+    }).then((user) => {
+        return createJWT(user)
+            .then((token) => {
+                res.json({
+                    "status": true,
+                    "message": "회원가입이 완료됐습니다.",
+                    "token": token
+                })
+            })
+            .catch((err) => {
+                res.json({
+                    "status": false,
+                    "message": "회원가입이 실패했습니다.",
+                    "error": err
+                })
+            })
+
     }).catch((err) => {
         res.json({
             "status": false,
@@ -43,17 +69,9 @@ router.post('/login', async function(req, res, next) {
             "message": "로그인에 실패했습니다."
         })
     } else {
-        const secret_key = "ssaudy";
-        const payload = {
-            "sub": "ssaudy",
-            "id": user.id,
-            "username": user.username,
-            "address": user.address,
-            "location": user.location,
-            "phone_number": user.phone_num,
-        };
-        const token = jwt.sign(payload, secret_key);
+        const token = await createJWT(user);
         res.json({
+            "status": true,
             "jwt": token
         })
     }
