5851d5657e0abfac19629eef3f1b5bb5b2f031f1
jisssh <tkdgus2683@gmail.com>
2019-10-15 12:15:38 +0900

    S1P1251014-25 메인페이지 - 푸터

--- frontend/web/yogiyo/src/App.vue
+++ frontend/web/yogiyo/src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app id="app">
+    <v-content>
+      <router-view />
+      <Footer></Footer>
+    </v-content>
+</v-app>
+
 </template>
 
 <script>
-
+import Footer from './components/Footer'
 export default {
   name: 'app',
   components: {
-    
+      Footer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
