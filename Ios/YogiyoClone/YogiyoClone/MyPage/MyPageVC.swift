a2d4b0912a3032bbf31ec4b43e242f4de77238ff
Jinyung Yoon <jny4594@gmail.com>
2019-10-28 08:54:23 +0000

    Update Ios/YogiyoClone/YogiyoClone/MyPage/MyPageVC.swift

--- Ios/YogiyoClone/YogiyoClone/MyPage/MyPageVC.swift
+++ Ios/YogiyoClone/YogiyoClone/MyPage/MyPageVC.swift
@@ -13,6 +13,7 @@ class MyPageVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     } 
 
